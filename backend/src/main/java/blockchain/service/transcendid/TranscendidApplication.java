28b2ab51afcec0cf778f3d3348da7ee96cb21ec4
U200709027 <okancaglar7@gmail.com>
2025-01-05 16:27:24 +0300

    project setup

---
+++ backend/src/main/java/blockchain/service/transcendid/TranscendidApplication.java
@@ -0,0 +1,13 @@
+package blockchain.service.transcendid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranscendidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranscendidApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/blockchain/service/transcendid/TranscendidApplicationTests.java
@@ -0,0 +1,13 @@
+package blockchain.service.transcendid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranscendidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
